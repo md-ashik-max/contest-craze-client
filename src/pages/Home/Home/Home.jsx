188500e8ab8232b47b9033f27990de5f7391fb4e
md-ashik-max <ar1214697@gmail.com>
2024-06-01 16:25:22 +0600

    added basic routing setup and navbar

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../Shared/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is home</h2>
+            
+        </div>
+    );
+};
+
+export default Home;
